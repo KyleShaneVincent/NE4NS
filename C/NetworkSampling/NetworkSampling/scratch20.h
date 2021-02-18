de5be66275e2f09867c67eef2f39ae7499ed0a5a
Jacques <jacques@synthesis.co.za>
2021-02-18 09:52:03 -0500

    added parameters to method

--- C/NetworkSampling/NetworkSampling/scratch20.h
+++ C/NetworkSampling/NetworkSampling/scratch20.h
@@ -8,4 +8,6 @@
 #define NETWORKSAMPLING_API __declspec(dllimport)
 #endif
 
-extern __declspec(dllexport) int __cdecl scratch20();
+extern __declspec(dllexport) int __cdecl scratch20(int *tsteps_value, double *paad0_value, int *naad0_value, double *paadtrace_value,
+												   int *aadreseeddesign_value, int *paadrandom_value, int *naadtarget, int *coupons_value,
+												   char *outputfilepath, char *nodeinputfilepath, char *nodefiinputfilepath, char *edgeinputfilepath);
